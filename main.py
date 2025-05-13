aa6ba160ff368792abc34f0826b8c814bc7c9e11
Ellie Alavi <elhamalavi.f@gmail.com>
2025-05-13 17:43:32 -0400

    comments from Ansel

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 '''
 Clarifying question
-1- Should all items in mat be the same length? Yes
+1- Should all items in mat be the same length? Yes (if not the same then it's called jagged array not a matrix)
 2- How does the function handle the invalid input? It returns the original array
+(3- it is important to recognize empty matrix:
+"I am going to assume matrices are not empty because of the contriants")
+(4- recognize the output different than original matrix if wrong input)
+** 5- the list's items could be anything:Str, other arrays, etc.
 Here are some example clarifying questions:
     Are there any constraints on matrix sizes?
     Can a matrix have 1 row or 1 column?
@@ -15,10 +19,28 @@ Here are some example clarifying questions:
 # checks if the number of columns in output matrix a divisor of row * column.
 # Makes a consolidated list of all items in one row (flatten the list)
 # Slices the full length list into smaller arrays with length = new column size (c)
+# Complexity: if n=row and m=col then O(n*m) is the complexity 
+# but it is actually linear. it is just POV that shows the sahpe of data tha's 2D
 
+
+# multiplification always preferred over division cause division's slower, and the ZeroDivision Error danger!
 def get_divisors(n):
-    return [i for i in range(1, n + 1) if n % i == 0]
+    return [i for i in range(1, n + 1) if n % i == 0]  
 
+def flatten(mat):
+    flat = [x for row in mat for x in row] # list comprehension
+
+    # flat = []
+    # for row in mat:
+    #     for x in row:
+    #         flat.append(x)
+
+    # Option 2: appending lists:
+    # flat = []
+    # for sub_array in mat:
+    #     flat += sub_array
+    
+    return flat
 
 def reshape_matrix(mat, r,c):
     
@@ -29,7 +51,7 @@ def reshape_matrix(mat, r,c):
     if rows not in range(100) or cols not in range(100):
         raise ValueError("Please input a matrix with maximum size of 100 x 100")
 
-    if c not in get_divisors(length) or r * c > length:
+    if c not in get_divisors(length) or r * c > length: # instead of division check if r*c same as length of flat
         return mat
     
     if any(len(item) != len(mat[0]) for item in mat):
@@ -37,12 +59,7 @@ def reshape_matrix(mat, r,c):
     
     # Flatten the original 2D list
     # SC: O(n) = n, TC: O(n)= n
-    flat = [x for row in mat for x in row] # list comprehension
-
-    # flat = []
-    # for row in mat:
-    #     for x in row:
-    #         flat.append(x)
+    flat = flatten(mat)
 
     # Reshape into r x c
     reshaped = [flat[i:i+c] for i in range(0, len(flat), c)]
--- tests/test_01.py
+++ tests/test_01.py
@@ -1,7 +1,7 @@
 from main import reshape_matrix
 import pytest
 
-def test_reshape_matrix_valid_case():
+def test_reshape_4_2_to_2_4():
     nums = [[1,2],
             [3,4],
             [5,6],
@@ -14,13 +14,25 @@ def test_reshape_matrix_valid_case():
     result = reshape_matrix(nums, r, c)
 
     assert result == expected
-
+    # We can also asser:
+    assert result is not nums
+    assert result(id) == expected(id)
 
 def test_invalid_matrix_size():
     nums = [[1,2,3],[3,7]]
     r = 3
     c = 2
     expected = [[1,2,3],[3,7]]
+
+    result = reshape_matrix(nums, r, c)
+
+    assert result == expected
+
+def test_indivisable_matrix_size():
+    nums = [[1,2,3],[3,7,9]]
+    r = 1
+    c = 5
+    expected = [[1,2,3],[3,7,9]]
 
     result = reshape_matrix(nums, r, c)
 
@@ -36,3 +48,5 @@ def test_empty_matrix():
     result = reshape_matrix(nums, r, c)
 
     assert result == expected
+
+print("All Passed!")  # A good statement to include at the end of our tests
